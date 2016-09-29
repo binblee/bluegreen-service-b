de6e31f8a77ea8e6fd579c46fa1a32323630d6db
Li Bin <binblee@users.noreply.github.com>
2016-09-29 18:31:49 +0800

    app.py 'Greetings'

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     msg = requests.get(dep_endpoint);
-    return 'Hello from ' + msg.text;
+    return 'Greetings from ' + msg.text;
 
 if __name__ == '__main__':
         app.run(port=5001,debug=True,host='0.0.0.0')
