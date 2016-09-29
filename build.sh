8b6c1b8ae53613b9f939095d73ead817cf33c2da
Li Bin <binblee@users.noreply.github.com>
2016-09-29 12:18:51 +0800

    fix typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git_hash=$(git rev-parse --short HEAD)
 docker build -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-b:${git_hash} .
-image_id=$(docker build -q -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-b:${git_hash})
+image_id=$(docker build -q -t registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-b:${git_hash} .)
 docker tag ${image_id} registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-b:latest
 docker push registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-b:${git_hash}
 docker push registry.cn-hangzhou.aliyuncs.com/jingshanlb/bluegreen-service-b:latest
